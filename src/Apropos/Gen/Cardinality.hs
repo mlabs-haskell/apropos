50d974e7ec2c94fcb05583c6a8055654bfea74d2
Fraser Dunlop <fraser@mlabs.city>
2022-02-27 15:39:53 +0000

    we can interpret Gen as an enumerator

--- src/Apropos/Gen/Cardinality.hs
+++ src/Apropos/Gen/Cardinality.hs
@@ -4,17 +4,31 @@ module Apropos.Gen.Cardinality (
   genCardinality,
   ) where
 import Apropos.Gen
---import Control.Monad.Free
+import Apropos.Gen.Range
+import Control.Monad.Free
 
 data Cardinality =
-    Contiguous Range
+    Scalar Integer
   | Product [Cardinality]
   | Sum [Cardinality]
 
 class HasCardinality t where
   cardinality :: t -> Cardinality
-  cardinality _ = Contiguous (singleton 1)
+  cardinality _ = Scalar 1
 
+unit :: Cardinality
+unit = Scalar 1
 
-genCardinality :: Gen a -> Cardinality
+_range :: Range -> Cardinality
+_range (Singleton _) = Scalar 1
+_range (Linear a b) = Scalar $ fromIntegral $ b - a
+
+genCardinality :: forall a . Gen a -> Cardinality
+genCardinality (Free (Label _ next)) = genCardinality next
+genCardinality (Free (FailWithFootnote _ _)) = unit
 genCardinality _ = undefined
+--genCardinality (Free (GenBool next)) = genCardinality (next (Scalar 2))
+--genCardinality (Free (GenInt r next)) = 
+
+--the thought here is to compute the size of the codomain of a generator morphism
+

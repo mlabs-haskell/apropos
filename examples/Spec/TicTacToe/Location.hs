9415107594746a89a26fbd9c320e578f7fb3bfff
Jack Hodgkinson <30505104+jhodgdev@users.noreply.github.com>
2022-03-23 10:56:03 +0000

    Added documentation strings to TicTacToe Location and Player modules

--- examples/Spec/TicTacToe/Location.hs
+++ examples/Spec/TicTacToe/Location.hs
@@ -1,3 +1,7 @@
+{- |
+Module: Spec.TicTacToe.Location
+Description: Defines logical properties of locations on a TicTacToe grid.
+-}
 module Spec.TicTacToe.Location (
   LocationProperty (..),
   locationPermutationGenSelfTest,
@@ -13,33 +17,48 @@ import GHC.Generics (Generic)
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.Hedgehog (fromGroup)
 
+-- | Properties of a "Location" (or grid space) in a game of TicTacToe.
 data LocationProperty
-  = LocationIsWithinBounds
-  | LocationIsOutOfBounds
+  = -- | Proposed location is inside 3x3 grid.
+    LocationIsWithinBounds
+  | -- | Proposed locations is outside of 3x3 grid.
+    LocationIsOutOfBounds
   deriving stock (Eq, Ord, Enum, Show, Bounded, Generic)
   deriving anyclass (Enumerable)
 
 instance LogicalModel LocationProperty where
+  -- Only relation between the two 'LocationProperty's is that
+  -- they may not both hold at once.
   logic = ExactlyOne $ Var <$> [LocationIsWithinBounds, LocationIsOutOfBounds]
 
+{- | TicTacToe grid spaces may be conceived of as an integer
+     between 0 and 8.
+-}
 instance HasLogicalModel LocationProperty Int where
   satisfiesProperty LocationIsWithinBounds location = location >= 0 && location < 9
   satisfiesProperty LocationIsOutOfBounds location =
     not (satisfiesProperty LocationIsWithinBounds location)
 
+{- | Any space out of bounds may be altered to be within
+     bounds and vice versa.
+-}
 instance HasPermutationGenerator LocationProperty Int where
   generators =
     [ Morphism
         { name = "MakeLocationIsWithinBounds"
         , match = Var LocationIsOutOfBounds
         , contract = remove LocationIsOutOfBounds >> add LocationIsWithinBounds
-        , morphism = \_ -> int (linear 0 8)
+        , -- Whatever the value is, replace it with a number between
+          -- zero and eight.
+          morphism = \_ -> int (linear 0 8)
         }
     , Morphism
         { name = "MakeLocationIsOutOfBounds"
         , match = Var LocationIsWithinBounds
         , contract = remove LocationIsWithinBounds >> add LocationIsOutOfBounds
-        , morphism = \_ ->
+        , -- Replace the number with some value between `minBound` and -1
+          -- or 9 and `maxBound`.
+          morphism = \_ ->
             choice
               [ int (linear minBound (-1))
               , int (linear 9 maxBound)
@@ -50,9 +69,15 @@ instance HasPermutationGenerator LocationProperty Int where
 instance HasParameterisedGenerator LocationProperty Int where
   parameterisedGenerator = buildGen baseGen
 
+{- | A generator for any `Int` between minBound and maxBound
+     i.e. all `Int`s.
+-}
 baseGen :: Gen Int
 baseGen = int (linear minBound maxBound)
 
+{- | Test that ensures all valid property combinations can
+     be generated successfully.
+-}
 locationPermutationGenSelfTest :: TestTree
 locationPermutationGenSelfTest =
   testGroup "locationPermutationGenSelfTest" $
--- examples/Spec/TicTacToe/Player.hs
+++ examples/Spec/TicTacToe/Player.hs
@@ -1,3 +1,7 @@
+{- |
+Module: Spec.TicTacToe.Player
+Description: TicTacToe player properties and model.
+-}
 module Spec.TicTacToe.Player (
   PlayerProperty (..),
   playerPermutationGenSelfTest,
@@ -12,40 +16,56 @@ import Apropos.LogicalModel
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.Hedgehog (fromGroup)
 
+-- | Properties which may be true of a TicTacToe player.
 data PlayerProperty
-  = PlayerIsX
-  | PlayerIsO
-  | PlayerIsInvalid
+  = -- | Player is playing crosses.
+    PlayerIsX
+  | -- | Player is playing noughts.
+    PlayerIsO
+  | -- | Player is invalid.
+    PlayerIsInvalid
   deriving stock (Eq, Ord, Enum, Show, Bounded)
 
 instance Enumerable PlayerProperty where
   enumerated = [minBound .. maxBound]
 
+-- | Relationships between `PlayerProperty`s.
 instance LogicalModel PlayerProperty where
   logic = ExactlyOne $ Var <$> [PlayerIsInvalid, PlayerIsX, PlayerIsO]
 
+-- | The properties of a player may be defined with `Int`s.
 instance HasLogicalModel PlayerProperty Int where
+  -- Player is crosses if player :: Int == 1.
   satisfiesProperty PlayerIsX player = player == 1
+  -- Player is noughts if player :: Int == 0.
   satisfiesProperty PlayerIsO player = player == 0
+  -- All other values of player :: Int are considered invalid.
   satisfiesProperty PlayerIsInvalid player =
     not (satisfiesAny [PlayerIsX, PlayerIsO] player)
 
+{- | Any player :: `Int` can be transformed into a different type
+     of player.
+-}
 instance HasPermutationGenerator PlayerProperty Int where
   generators =
     [ Morphism
-        { name = "MakePlayerX"
+        { -- To make a player crosses, set their value to 1.
+          name = "MakePlayerX"
         , match = Not $ Var PlayerIsX
         , contract = removeAll [PlayerIsO, PlayerIsInvalid] >> add PlayerIsX
         , morphism = \_ -> pure 1
         }
     , Morphism
-        { name = "MakePlayerO"
+        { -- To make a player noughts, set their value to 0.
+          name = "MakePlayerO"
         , match = Not $ Var PlayerIsO
         , contract = removeAll [PlayerIsX, PlayerIsInvalid] >> add PlayerIsO
         , morphism = \_ -> pure 0
         }
     , Morphism
-        { name = "MakePlayerInvalid"
+        { -- To make a player invalid, set their value to anything
+          -- other than zero or one.
+          name = "MakePlayerInvalid"
         , match = Not $ Var PlayerIsInvalid
         , contract = removeAll [PlayerIsX, PlayerIsO] >> add PlayerIsInvalid
         , morphism = \_ ->
@@ -57,9 +77,11 @@ instance HasPermutationGenerator PlayerProperty Int where
 instance HasParameterisedGenerator PlayerProperty Int where
   parameterisedGenerator = buildGen baseGen
 
+-- | Generator for all `Int`s.
 baseGen :: Gen Int
 baseGen = int (linear minBound maxBound)
 
+-- | Tests ensuring that the model is sound.
 playerPermutationGenSelfTest :: TestTree
 playerPermutationGenSelfTest =
   testGroup "playerPermutationGenSelfTest" $

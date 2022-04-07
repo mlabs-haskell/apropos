565f501bb66de1e9acec417a357065f979d9e37c
Geometer1729 <16kuhnb@gmail.com>
2022-04-07 14:44:38 -0400

    add some path benchmarks

--- bench/Main.hs
+++ bench/Main.hs
@@ -5,12 +5,17 @@ import Apropos
 import Apropos.LogicalModel
 import Hedgehog
 import System.Environment (setEnv)
+import Data.DiGraph ( shortestPathCache,shortestPath_ )
+import Data.Maybe
 
 main :: IO ()
 main = do
   setEnv "HEDGEHOG_VERBOSITY" "0"
   defaultMain
     [ bench "rational sat" $ nf (length . enumerateSolutions) (logic @RatProp)
+    , bench "make cache" $ whnf (shortestPathCache . buildGraph) (findMorphisms (Apropos :: Rat :+ RatProp))
+    , bench "find paths" $ whnf (\c -> length . concat . catMaybes $ [ shortestPath_ f t c | f <- scenarios @RatProp , t <- scenarios ]) (shortestPathCache (buildGraph (findMorphisms (Apropos :: Rat :+ RatProp))))
     , bench "full rational test" $ nfIO
       $ checkParallel $ runGeneratorTestsWhere (Apropos :: Rat :+ RatProp) "generator" Yes
     ]
+

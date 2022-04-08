28f779d03a68741ba8bdb790ea901afd785809d6
Jack Hodgkinson <30505104+jhodgdev@users.noreply.github.com>
2022-04-08 10:57:58 +0100

    Added docstring for HasPurerunner

--- src/Apropos/Pure.hs
+++ src/Apropos/Pure.hs
@@ -11,11 +11,33 @@ import Data.Set qualified as Set
 import Data.String (fromString)
 import Hedgehog (Group (..), Property, TestLimit, property, withTests, (===))
 
+{- | Typeclass permitting model/property pairings to be run.
+
+     Any value for which `script` returns `True` is expected to
+     satisfy the `Formula` defined by `expect`.
+
+     Simultaneously, any value for which the `expect` `Formula`
+     is /unsatisfied/, should return `False`, when run by `script`.
+-}
 class
   (HasLogicalModel p m, HasParameterisedGenerator p m) =>
   HasPureRunner p m
   where
+  -- | `expect` defines the expected behaviour of `script`. This
+  --   relation must hold in both the positive and negative
+  --   contexts. That is to say:
+  --
+  --   - When the `expect` expression /is/ satisfied by the
+  --     properties of a value `m` then `script` must evaluate to 
+  --     `True`.
+  --
+  --   - When the `expect` expression /is not/ satisfied by the
+  --     properties of a value `m` then `script` must evaluate to 
+  --     `False`.
   expect :: m :+ p -> Formula p
+
+  -- | If a value `m` satisfies `expect`, then `script` should 
+  --   return `True`. Otherwise, it should return `False`.
   script :: m :+ p -> (m -> Bool)
 
   runPureTest :: m :+ p -> Set p -> Property

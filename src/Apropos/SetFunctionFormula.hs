0f0a91171729ca5169e2ee5728a3da41579354a8
Fraser Dunlop <fraser@mlabs.city>
2022-04-12 13:38:54 +0100

    format/refactor

--- src/Apropos/SetFunctionFormula.hs
+++ src/Apropos/SetFunctionFormula.hs
@@ -1,25 +1,27 @@
 module Apropos.SetFunctionFormula (
-    findEdges
-  , adds
-  , removes
-  , SetFunctionLanguage
-  ) where
-import Apropos.LogicalModel.Formula
+  findEdges,
+  adds,
+  removes,
+  SetFunctionLanguage,
+) where
+
 import Apropos.LogicalModel.Enumerable
-import Data.Set (Set)
-import Data.Set qualified as Set
-import Data.Map (Map)
-import Data.Map qualified as Map
+import Apropos.LogicalModel.Formula
 import Control.Monad (join)
-import Data.Maybe (fromMaybe)
 import Control.Monad.Free
 import Control.Monad.State (State, execState, get, put)
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Data.Maybe (fromMaybe)
+import Data.Set (Set)
+import Data.Set qualified as Set
 
 findEdges :: Enumerable a => Formula a -> SetFunctionLanguage a () -> Set (Set a, Set a)
 findEdges matches expr = Set.fromList changes
-  where sols = solveAll $ (I <$> matches) :&&: setFunctionToFormula expr
-        tras = solutionToSetTranslation <$> sols
-        changes = filter (uncurry (/=)) tras
+  where
+    sols = solveAll $ (I <$> matches) :&&: setFunctionToFormula expr
+    tras = solutionToSetTranslation <$> sols
+    changes = filter (uncurry (/=)) tras
 
 adds :: a -> SetFunctionLanguage a ()
 adds a = liftF (Adds a ())
@@ -27,9 +29,9 @@ adds a = liftF (Adds a ())
 removes :: a -> SetFunctionLanguage a ()
 removes a = liftF (Removes a ())
 
-data FreeSetFunctionLanguage a next =
-      Adds a next
-    | Removes a next
+data FreeSetFunctionLanguage a next
+  = Adds a next
+  | Removes a next
 
 instance Functor (FreeSetFunctionLanguage a) where
   fmap f (Adds a next) = Adds a (f next)
@@ -43,26 +45,30 @@ setFunctionToFormula :: Enumerable a => SetFunctionLanguage a () -> Formula (S a
 setFunctionToFormula = mapReprToFormula . setFunctionToMapRepr
 
 solutionToSetTranslation :: Enumerable a => Map (S a) Bool -> (Set a, Set a)
-solutionToSetTranslation sol = (i,o)
-  where i = Set.fromList [ x | x <- enumerated, fromMaybe (error "undefined variable") (Map.lookup (I x) sol) ]
-        o = Set.fromList [ x | x <- enumerated, fromMaybe (error "undefined variable") (Map.lookup (O x) sol) ]
+solutionToSetTranslation sol = (i, o)
+  where
+    i = Set.fromList [x | x <- enumerated, fromMaybe (error "undefined variable") (Map.lookup (I x) sol)]
+    o = Set.fromList [x | x <- enumerated, fromMaybe (error "undefined variable") (Map.lookup (O x) sol)]
 
 idSetFunctionMapRepr :: Enumerable a => Map (Formula a) (Formula a)
 idSetFunctionMapRepr =
-  Map.fromList $ join [ [ (Var x, Var x)
-                        , (Not (Var x), Not (Var x))
-                        ]
-                      | x <- enumerated
-                      ]
+  Map.fromList $
+    join
+      [ [ (Var x, Var x)
+        , (Not (Var x), Not (Var x))
+        ]
+      | x <- enumerated
+      ]
 
 setFunctionToMapRepr :: Enumerable a => SetFunctionLanguage a () -> Map (Formula a) (Formula a)
 setFunctionToMapRepr sfl = execState (evalSetFunctionOnMapRepr sfl) idSetFunctionMapRepr
 
 mapReprToFormula :: Map (Formula a) (Formula a) -> Formula (S a)
 mapReprToFormula mrep =
-  All [ (I <$> i) :->: (O <$> o)
-      | (i,o) <- Map.toList mrep
-      ]
+  All
+    [ (I <$> i) :->: (O <$> o)
+    | (i, o) <- Map.toList mrep
+    ]
 
 evalSetFunctionOnMapRepr :: Enumerable a => SetFunctionLanguage a () -> State (Map (Formula a) (Formula a)) ()
 evalSetFunctionOnMapRepr (Free (Adds a next)) = do
@@ -76,5 +82,3 @@ evalSetFunctionOnMapRepr (Free (Removes a next)) = do
   put m'
   evalSetFunctionOnMapRepr next
 evalSetFunctionOnMapRepr (Pure next) = pure next
-
-

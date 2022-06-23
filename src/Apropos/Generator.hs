e278fa47a70410454bcf45c85ebac05c45224474
Ari Fordsham <arifordsham@gmail.com>
2022-06-23 14:04:48 +0300

    Some implementation tidies

--- src/Apropos/Generator.hs
+++ src/Apropos/Generator.hs
@@ -1,31 +1,37 @@
 module Apropos.Generator (
   runTest,
+  filteredTests,
+  decorateTests,
   selfTest,
   selfTestWhere,
 ) where
 
-import Apropos.Description (DeepHasDatatypeInfo, Description (..), Attribute, enumerateScenariosWhere, variablesToDescription)
-import Apropos.Formula (Formula (..))
+import Apropos.Description (DeepHasDatatypeInfo, Description (..), variablesToDescription, scenarios)
 import Data.Set qualified as Set
 import Data.String (IsString, fromString)
 import Hedgehog (Property, PropertyT, forAll, property, (===))
+import Data.Set (Set)
 
 runTest :: (Show a, Description d a) => (a -> PropertyT IO ()) -> d -> Property
 runTest cond d = property $ forAll (genDescribed d) >>= cond
+
+filteredTests :: forall d a. (Description d a, DeepHasDatatypeInfo d, Ord d) => (d -> Bool) -> Set d
+filteredTests f = Set.filter f . Set.map variablesToDescription $ scenarios @d
+
+decorateTests :: (IsString s, Show d) => (d -> Property) -> Set d -> [(s, Property)]
+decorateTests f = map (\d -> (fromString $ show d, f d)) . Set.toList
 
 -- TODO caching calls to the solver in genSatisfying would probably be worth it
 selfTestForDescription :: forall d a. (Eq d, Show d, Show a, Description d a) => d -> Property
 selfTestForDescription d = runTest (\a -> describe a === d) d
 
 selfTest :: forall d a s. (Ord d, Show d, Show a, Description d a, DeepHasDatatypeInfo d, IsString s) => [(s, Property)]
-selfTest = selfTestWhere @d Yes
+selfTest = selfTestWhere @d (const True)
 
 selfTestWhere ::
   forall d a s.
   (Ord d, Show d, Show a, Description d a, DeepHasDatatypeInfo d, IsString s) =>
-  Formula (Attribute d) ->
+  (d -> Bool) ->
   [(s, Property)]
-selfTestWhere condition =
-  [ (fromString $ show $ variablesToDescription scenario, selfTestForDescription (variablesToDescription scenario))
-  | scenario <- Set.toList $ enumerateScenariosWhere condition
-  ]
+selfTestWhere = decorateTests selfTestForDescription . filteredTests
+
--- src/Apropos/Runner.hs
+++ src/Apropos/Runner.hs
@@ -4,12 +4,11 @@ module Apropos.Runner (
   runTestsWhere,
 ) where
 
-import Apropos.Description (DeepHasDatatypeInfo, Description (..), scenarios, variablesToDescription)
-import Apropos.Generator (runTest)
+import Apropos.Description (DeepHasDatatypeInfo, Description (..))
+import Apropos.Generator (runTest, filteredTests, decorateTests)
 import Control.Monad.Trans.Except (ExceptT (ExceptT), runExceptT)
 import Data.Either (isRight)
-import Data.Set qualified as Set
-import Data.String (IsString, fromString)
+import Data.String (IsString)
 import Hedgehog (Property, PropertyT, (===))
 import Hedgehog.Internal.Property (PropertyT (PropertyT), TestT (TestT, unTest), unPropertyT)
 
@@ -23,7 +22,7 @@ runAproposTest atest d =
   runTest
     ( \a -> do
         b <- passes (aproposTest atest a)
-        b === expect atest d
+        expect atest d === b
     )
     d
   where
@@ -41,9 +40,4 @@ runTests :: forall d a s. (Show d, Show a, Ord d, Description d a, DeepHasDataty
 runTests = runTestsWhere (const True)
 
 runTestsWhere :: forall d a s. (Show d, Show a, Ord d, Description d a, DeepHasDatatypeInfo d, IsString s) => (d -> Bool) -> AproposTest d a -> [(s, Property)]
-runTestsWhere cond atest =
-  [ ( fromString $ show scenario
-    , runAproposTest atest scenario
-    )
-  | scenario <- Set.toList . Set.filter cond . Set.map variablesToDescription $ scenarios
-  ]
+runTestsWhere cond atest = decorateTests (runAproposTest atest) $ filteredTests cond
